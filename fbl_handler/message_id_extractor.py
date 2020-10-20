3ad23d4c61eae2db437d33ba31b7da12e0ca3cc8
Yermakov Kirill <yermakov.kirill@gmail.com>
2020-10-20 17:40:44 +0300

    Обработчик fbl

--- fbl_handler/message_id_extractor.py
+++ fbl_handler/message_id_extractor.py
@@ -1,13 +1,18 @@
 import re
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class MessageIdExtractor():
 
     def extract_from_yandex_fbl(self, message_content: str) -> str:
-        groups = re.search(".*bounce@togas-shop\.ru>\\)\sid\s(.+?)\s.*", message_content,
-                           flags=re.MULTILINE | re.UNICODE).groups()
+        search = re.search(".*" + os.getenv('fbl_pattern_yandex') + "\.ru>\\)\sid\s(.+?)\s.*", message_content,
+                           flags=re.MULTILINE | re.UNICODE)
 
-        if groups is None:
+        if search is None:
             return ''
 
-        return groups[0]
+        return search.groups()[0]
+
+
